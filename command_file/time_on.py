f48c2aba3a17aefdc13add7c35de82ba2dffe255
wantZzz <88297290+wantZzz@users.noreply.github.com>
2022-10-22 17:48:29 +0800

    Delete daily memes function
    Can't work because meme.tw has anti-crawlers

--- command_file/time_on.py
+++ command_file/time_on.py
@@ -56,48 +56,7 @@ class time_on(Cog_Extension):
             await self.channel.send("大家晚安晚安www")
           await asyncio.sleep(60)
         elif (now == 720+time_on.sleep_time):#say goodmorning
-          with open('users_datafile/state.json') as f:
-            state = json.load(f)
-          if state['noise']:
-            with open('author_datafile/meme_type_list.json') as e:
-              meme_types = json.load(e)
-        
-            meme_api = requests.get('https://memes.tw/wtf/api')#add hot meme from memes.tw
-
-            if meme_api.status_code == requests.codes.ok:
-              new_hotmeme = meme_api.json()
-              new_hotmeme.sort(key = lambda s: (1+(s["total_like_count"]/s['pageview']))*s["pageview"], reverse = True)#sort out hottest
-
-              keys = list(meme_types.keys())
-
-              counter = 0
-              while keys.index(new_hotmeme[counter]["contest"]["name"])+1 in state['ban_meme_id']:
-                counter += 1
-                if not new_hotmeme[counter]["contest"]["name"] in keys:
-                  break
-                  
-              if counter >= len(new_hotmeme):#if all hot meme can't public in Non-nsfw channel
-                await self.channel.send("早安安!Wah! 最近似乎太多色色的了...")
-
-              else:
-                hotest = new_hotmeme[counter]
-
-                if hotest["contest"]["name"] in keys:
-                  id_num = keys.index(hotest["contest"]["name"])+1
-                else:
-                  id_num = "冷門類別無收錄"
-  
-                embed = discord.Embed(title=f"{hotest['title']} | 每日梗圖",color=0xffffff)
-                embed.add_field(name='類別:', value=f'{hotest["contest"]["name"]} ({id_num})')
-                embed.add_field(name='來源:', value=hotest["url"])
-                embed.set_image(url=hotest["src"])
-                embed.set_footer(text=f"powerby:https://memes.tw/ | {hotest['created_at']['date_time_string']}",icon_url='https://memes.tw/ms-icon-310x310.png')
-
-                await self.channel.send("早安安!Wah!", embed=embed)
-
-            else:
-              await self.channel.send("早安安!Wah!")
-
+          await self.channel.send("早安安!Wah!")
           await asyncio.sleep(60)
 
         else:
